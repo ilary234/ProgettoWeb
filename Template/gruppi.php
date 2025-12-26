14e100cf09ee51382b1d64260469370ab862d057
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2025-12-26 23:20:19 +0100

    Added gruppi.php for the main structure of the page gruppi

---
+++ Template/gruppi.php
@@ -0,0 +1,17 @@
+<section>
+    <form action="#" class="row text-center">
+        <div class="col-12">
+            <div class="row justify-content-sm-center">
+            <label for="corsi" class="col-2 col-sm-auto">Filtra: </label>
+            <select id="corsi" name="corsi" class="col-3">
+                <option value="--">--</option>
+            </select>
+            <label for="materie" class="col-1 col-sm-auto"></label>
+            <select id="materie" name="materie" class="col-3">
+                <option value="--">--</option>
+            </select>
+            </div>
+        </div>
+    </form>
+</section>
+<section class="flex-container"></section>
