dfb4a236621546e83803383671c2e18e10a65bfe
Matilde Mariani <matilde.mariani3@studio.unibo.it>
2025-12-31 01:28:50 +0100

    Fixed references

--- Template/base.php
+++ Template/base.php
@@ -11,9 +11,15 @@
             <div class="row">
                 <nav class="col-12">
                     <ul class="row nav text-center">
-                        <li class="nav-item col-3 col-sm-auto"><a class="nav-link" href="#">Gruppi</a></li>
-                        <li class="nav-item col-3 col-sm-auto"><a class="nav-link" href="#">Annunci</a></li>
-                        <li class="nav-item col-3 col-sm-auto ms-auto"><a class="nav-link" href="#">Login</a></li>
+                        <li class="nav-item col-3 col-sm-auto"><a class="nav-link" href="index.php">Gruppi</a></li>
+                        <li class="nav-item col-3 col-sm-auto"><a class="nav-link" href="annunci.php">Annunci</a></li>
+                        <li class="nav-item col-3 col-sm-auto ms-auto">
+                            <?php if(isset($_SESSION['username'])): ?>
+                                <a class="nav-link" href="areaRiservata.php">Login</a>
+                            <?php else: ?>
+                                <a class="nav-link" href="login.php">Login</a>
+                            <?php endif; ?>
+                        </li>
                     </ul>
                 </nav>
             </div>
