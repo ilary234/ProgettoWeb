086605ecb0f6a403866f39575dbc9179c3cb38d0
Matilde Mariani <matilde.mariani3@studio.unibo.it>
2025-12-31 01:32:03 +0100

    Added annunci.php

---
+++ annunci.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'bootstrap.php';
+    $templateParams["nome"] = "Template/annunci.php";
+    $templateParams["js"] = array("JS/annunci.js");
+    require './Template/base.php';
+?>
